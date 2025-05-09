34e875e5d93482a94bd0d3887bc7b2d270459be2
Damian Dalle Nogare <damian.dalle@fht.org>
2025-05-09 16:52:13 +0200

    updated analysis recursive

--- analysis_recursive.py
+++ analysis_recursive.py
@@ -44,35 +44,56 @@ def load_image(file_path):
     im = loaded_im.data[0, :, 0, :, :]
     return im
 
+
+def make_organoid_mask(filename, output_folder, image_path, seg_im):
+            
+    os.makedirs(os.path.join(output_folder, 'organoid_masks'), exist_ok=True)
+    temp_im = np.zeros(seg_im.shape)
+    raw_files = os.listdir(image_path)
+    for folder in raw_files:
+        if 'dapi' not in folder:
+            ch_file = skimage.io.imread(os.path.join(image_path, folder, 'bgsub_' + filename[:-4] + '_ch' + str(folder[-1]) + '.tif')) 
+            temp_im = temp_im + ch_file
+        else:
+            dapi_file = skimage.io.imread(os.path.join(output_folder, 'raw_images', folder, filename[:-4] + '_dapi.tif')) 
+            temp_im = temp_im + dapi_file
+    blurred = skimage.filters.gaussian(temp_im, 3)
+    thresh = skimage.filters.threshold_triangle(blurred)
+    binary = blurred > thresh  
+    binary = np.logical_or(binary, seg_im)
+    binary[binary > 0] = 1
+
+    h, w = binary.shape
+    border_mask = np.ones_like(binary, dtype=bool)
+    border_mask[int(h*0.1):int(h*0.9), int(w*0.1):int(w*0.9)] = False
+    labs = skimage.measure.label(binary)
+    labs = labs.astype(np.uint16)
+    border_labs = np.unique(labs[border_mask])
+    for border in border_labs:
+        labs[labs == border] = 0
+
+    labs[labs > 0] = 1
+
+    return labs
+
+
 def process_images(im, filename, raw_savefolder, bg_sub_folder, dapi_channel):
 
     for channel in range(0, im.shape[0]):
-        #print('starting preprocessing for channel ' + str(channel+1))
         if channel == dapi_channel:
             skimage.io.imsave(raw_savefolder + os.path.sep + 'channel_dapi' + os.path.sep + filename[:-4] + '_dapi.tif', im[channel,:,:].astype(np.uint16), check_contrast=False)
-            #print('finished saving DAPI raw')
+            print('finished saving DAPI raw')
         else:
             skimage.io.imsave(raw_savefolder + os.path.sep + 'channel_' + str(channel+1) + os.path.sep + 'raw_' + filename[:-4] + '_ch' + str(channel+1) + '.tif', im[channel,:,:].astype(np.uint16), check_contrast=False)
-            #print('finished saving raw')
-           
-            #tiles = da.from_array(im[channel,:,:], chunks=(512, 512))
-            #tile_map = da.map_blocks(tophat_process, tiles)
-            #sub_im = tile_map.compute()
-            sub_im = tophat_process(im[channel,:,:].astype(np.uint16))
-            #print('finished subtraction')
-
-            skimage.io.imsave(bg_sub_folder + os.path.sep + 'channel_' + str(channel+1) + os.path.sep + 'bgsub_' + filename[:-4] + '_ch'  + str(channel+1) + '.tif', sub_im, check_contrast=False)
-            #print('finished saving subtraction')
+            print('finished saving raw channel ' + str(channel))
+            
+            pathfile = Path(os.path.join(bg_sub_folder + os.path.sep + 'channel_' + str(channel+1) + os.path.sep + 'bgsub_' + filename[:-4] + '_ch'  + str(channel+1) + '.tif'))
+            if pathfile.exists():
+                print('file exists, skipping')
+            else:
+                sub_im = tophat_process(im[channel,:,:].astype(np.uint16))
+                skimage.io.imsave(bg_sub_folder + os.path.sep + 'channel_' + str(channel+1) + os.path.sep + 'bgsub_' + filename[:-4] + '_ch'  + str(channel+1) + '.tif', sub_im, check_contrast=False)
       
-
-#raw_folder = easygui.diropenbox('Select raw data folder')
-#output_folder = easygui.diropenbox('Select folder to store results in')
-#dapi_channel = easygui.integerbox("Enter the DAPI chanenl (0 indexed)", "DAPI Channel", 3, 0, 5)
-
-#raw_folder = '/facility/imganfacusers/Testa/Claudio/20240508singlescene'
-#output_folder = '/facility/imganfacusers/Testa/Claudio/ss_hpc'
-#dapi_channel = 3
-
 
 CONFIG_NAME = 'config.yaml'
 
@@ -83,13 +104,12 @@ raw_folder = config['raw_folder']
 output_folder = config['output_folder']
 dapi_channel = config['dapi_channel']
 cellpose_model = config['cellpose_model_czi']
-print(raw_folder)
-print(output_folder)
-print(dapi_channel)
-
-#cellpose_model = '/facility/imganfac/neurogenomics/Testa/Claudio/scripts/models/Claudio_czi'
+print('raw folder: ' + str(raw_folder))
+print('output folder: ' + str(output_folder))
+print('dapi channel: ' + str(dapi_channel))
 
 file_paths = glob.glob(raw_folder + os.path.sep + '**/*.' + 'czi', recursive=True)
+
 
 temp_im = load_image(file_paths[0])
 num_channels = temp_im.shape[0]
@@ -101,7 +121,7 @@ for channel in range(0, num_channels):
     if channel != dapi_channel:
         channels_to_quantify.append(channel)
 
-  #make output folders
+#make output folders
 raw_image_folder = os.path.join(output_folder, 'raw_images')
 bg_sub_folder =  os.path.join(output_folder, 'preprocessed_images')
 masks_folder = os.path.join(output_folder, 'segmentation')
@@ -111,17 +131,14 @@ os.makedirs(masks_folder, exist_ok=True)
 
 start = time.time()
 for num, file in enumerate(file_paths):
-    if num < 99999:
+    if num < 9999:
         filename = os.path.basename(file)
-        pathfile = Path(os.path.join(output_folder, 'segmentation', 'seg_' + filename[:-4] + '.tif'))
-        #print(pathfile)
+
+        
+        pathfile = Path(os.path.join(output_folder, 'organoid_masks', 'organoid_mask_' + filename[:-4]  + '.tif'))
         if pathfile.exists():
             print('file exists, skipping')
         else:
-            
-          
-
-
             for i in range(0, num_channels):
                 if i == dapi_channel:
                     os.makedirs(raw_image_folder + os.path.sep + 'channel_dapi', exist_ok=True)
@@ -130,8 +147,6 @@ for num, file in enumerate(file_paths):
                     os.makedirs(raw_image_folder + os.path.sep + 'channel_' + str(i+1), exist_ok=True)
                     os.makedirs(bg_sub_folder + os.path.sep + 'channel_' + str(i+1), exist_ok=True)
                 
-
-
             #loop through and load files and save sum projections if needed
             im = load_image(file)
             print('pre-processing ' + filename)
@@ -143,9 +158,20 @@ for num, file in enumerate(file_paths):
 
             nuc_im = im[dapi_channel,:,:]
 
-            print('segmenting ' + filename)
-            masks, flows, styles  = model.eval(nuc_im, diameter=None, flow_threshold=None, channels=[0,0])
-            skimage.io.imsave(masks_folder + os.path.sep + 'seg_' + filename[:-4]  + '.tif', masks, check_contrast=False)                      
+            pathfile = Path(os.path.join(masks_folder + os.path.sep + 'seg_' + filename[:-4]  + '.tif'))
+            if pathfile.exists():
+                print('file exists, skipping')
+                masks = skimage.io.imread(masks_folder + os.path.sep + 'seg_' + filename[:-4]  + '.tif')
+            else:
+                print('segmenting ' + filename)
+                masks, flows, styles  = model.eval(nuc_im, diameter=None, flow_threshold=None, channels=[0,0])
+                skimage.io.imsave(masks_folder + os.path.sep + 'seg_' + filename[:-4]  + '.tif', masks, check_contrast=False)         
+
+
+            organoid_mask = make_organoid_mask(filename[:-4]  + '.tif', output_folder, bg_sub_folder, masks)
+            skimage.io.imsave(os.path.join(output_folder, 'organoid_masks', 'organoid_mask_' + filename[:-4]  + '.tif'), organoid_mask, check_contrast=False)
+
+
 end = time.time()
 elapsed = end - start
 print(f'Time taken: {elapsed:.6f} seconds')
